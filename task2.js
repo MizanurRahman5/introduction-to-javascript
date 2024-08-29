1245935e7396d9fdece990aba28d84d46ee30a0b
MistyIslam5 <mdmisty5@gmail.com>
2024-08-29 09:24:53 +0600

    task2 complet

---
+++ task2.js
@@ -0,0 +1,9 @@
+var bangla = 80;
+var english = 92.5  
+var mathematical = 95.2;
+var biology = 70;
+var physics = 95;
+
+var average = (bangla + english + mathematical + biology + physics) / 5;
+
+console.log("Average marks: " + average.toFixed(2));
